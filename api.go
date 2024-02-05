cb2c670cb234386632cfb5e93a2c1e3bf5baf68d
curious-neeraj <neerajjain.dev@gmail.com>
2024-02-05 17:00:44 +0530

    Add basic Handler code. Commit progress

--- api.go
+++ api.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+
+	return json.NewEncoder(w).Encode(v)
+}
+
+type ApiError struct {
+	Error string
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+// function to convert our apiFunc to Http.HandlerFunc
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			// handle error
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
 
 func apiServer() {
 	fmt.Println("Initialize API Server!")
 }
+
+type APIServer struct {
+	listenAddress string
+}
+
+func NewAPIServer(listenAddress string) *APIServer {
+	return &APIServer{
+		listenAddress: listenAddress,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+
+	log.Println("API Server running on port: ", s.listenAddress)
+	http.ListenAndServe(s.listenAddress, router)
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+// func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+// 	return nil
+// }
+
+// func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+// 	return nil
+// }
+
+// func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+// 	return nil
+// }
+
+// func (s *APIServer) handleTransaction(w http.ResponseWriter, r *http.Request) error {
+// 	return nil
+// }
